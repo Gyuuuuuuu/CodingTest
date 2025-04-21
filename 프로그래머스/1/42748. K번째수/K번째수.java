6dfd150a53902725b4c8667e0bcf6a5aead0423c
KIM MIN GYU <101085069+Gyuuuuuuu@users.noreply.github.com>
2025-04-21 16:35:16 +0900

    [level 1] Title: K번째수, Time: 0.33 ms, Memory: 70.1 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -8,14 +8,11 @@ class Solution {
             int start = commands[i][0] - 1;
             int end = commands[i][1];
             int k = commands[i][2] - 1;
-
-            // 배열 자르기
-            int[] temp = Arrays.copyOfRange(array, start, end);
             
-            // 정렬
+            int[] temp = Arrays.copyOfRange(array, start, end);
+                        
             Arrays.sort(temp);
             
-            // k번째 수
             answer[i] = temp[k];
         }
         
