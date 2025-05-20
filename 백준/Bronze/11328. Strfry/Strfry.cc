7c5bfeb0660f02a731653ef64791dfe30d530b72
KIM MIN GYU <101085069+Gyuuuuuuu@users.noreply.github.com>
2025-05-20 21:48:55 +0900

    [Bronze II] Title: Strfry, Time: 76 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/11328. Strfry/Strfry.cc
+++ 백준/Bronze/11328. Strfry/Strfry.cc
@@ -4,21 +4,22 @@ using namespace std;
 int main() {
     int test_case;
     cin >> test_case;
-
-    while (test_case--) {
+    for (int i = 0; i < test_case; i++) {
         string s1, s2;
         cin >> s1 >> s2;
 
-        sort(s1.begin(), s1.end());
-        sort(s2.begin(), s2.end());
-
-        if (s1 == s2) {
-            cout << "Possible\n";
-        }
-        else {
+        if (s1.length() != s2.length()) {
             cout << "Impossible\n";
         }
-    }
+        else {
+            sort(s1.begin(), s1.end());
+            sort(s2.begin(), s2.end());
 
+            if (s1 == s2)
+                cout << "Possible\n";
+            else
+                cout << "Impossible\n"; // 이 줄이 추가되어야 함
+        }
+    }
     return 0;
 }
