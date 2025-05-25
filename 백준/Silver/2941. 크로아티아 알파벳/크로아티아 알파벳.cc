2d6916c229bd26a7cd9846efe16281a5a605ce5f
KIM MIN GYU <101085069+Gyuuuuuuu@users.noreply.github.com>
2025-05-25 21:09:13 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.cc
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.cc
@@ -1,40 +1,34 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 
-    string s;
-    cin >> s;
+	string s;	
+	cin >> s;
 
-    int cnt = 0;
+	int cnt = s.length();
 
-    for (int i = 0; i < s.length(); i++) {
-        // "dz=" 처리 (가장 먼저)
-        if (i < s.length() - 2 && s[i] == 'd' && s[i + 1] == 'z' && s[i + 2] == '=') {
-            cnt++;
-            i += 2;
-        }
-        // 나머지 2글자 크로아티아 알파벳
-        else if (i < s.length() - 1) {
-            if ((s[i] == 'c' && (s[i + 1] == '=' || s[i + 1] == '-')) ||
-                (s[i] == 'd' && s[i + 1] == '-') ||
-                (s[i] == 'l' && s[i + 1] == 'j') ||
-                (s[i] == 'n' && s[i + 1] == 'j') ||
-                (s[i] == 's' && s[i + 1] == '=') ||
-                (s[i] == 'z' && s[i + 1] == '=')) {
-                cnt++;
-                i++;
-            } else {
-                cnt++;
-            }
-        }
-        // 일반 알파벳 1글자
-        else {
-            cnt++;
-        }
-    }
-
-    cout << cnt;
+	for (int i = 0; i < s.length(); i++) {
+		if (i < s.length() - 2 && s.length() >= 3) {
+			if (s[i] == 'd' && s[i + 1] == 'z' && s[i + 2] == '=') {
+				cnt -= 2;
+				i += 2;
+			}
+		}
+		if (i < s.length() - 1 && s.length() >=2) {
+			if (s[i] == 'c' && s[i + 1] == '=' ||
+				s[i] == 'c' && s[i + 1] == '-' ||
+				s[i] == 'd' && s[i + 1] == '-' ||
+				s[i] == 'l' && s[i + 1] == 'j' || 
+				s[i] == 'n' && s[i + 1] == 'j' || 
+				s[i] == 's' && s[i + 1] == '=' ||
+				s[i] == 'z' && s[i + 1] == '=') {
+				cnt--;
+				i++;
+			}
+		}		
+	}
+	cout << cnt;
 }
